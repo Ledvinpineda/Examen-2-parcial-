6f11f5cbd4d09c80db787785e32b7d11e6c64ad3
Ledvinp17 <ljperez1@uth.hn>
2024-11-23 13:16:48 -0600

    Examen segundo parcial

--- src/main/java/com/mycompany/gestionmedica_ljpp/Medico_LJPP.java
+++ src/main/java/com/mycompany/gestionmedica_ljpp/Medico_LJPP.java
@@ -8,8 +8,30 @@ package com.mycompany.gestionmedica_ljpp;
  *
  * @author perez
  */
-public class Medico_LJPP {
-    
-    public void class Persona_LJPP extends();
+public class Medico_LJPP extends Persona_LJPP {
+    private String especialidad_LJPP;
+    private int aniose_LJPP;
+
+    public Medico_LJPP(String especialidad_LJPP, int aniose_LJPP, String nombre_LJPP, int edad_LJPP, String direccion_LJPP) {
+        super(nombre_LJPP, edad_LJPP, direccion_LJPP);
+        this.especialidad_LJPP = especialidad_LJPP;
+        this.aniose_LJPP = aniose_LJPP;
+    }
+
+    public String getEspecialidad_LJPP() {
+        return especialidad_LJPP;
+    }
+
+    public void setEspecialidad_LJPP(String especialidad_LJPP) {
+        this.especialidad_LJPP = especialidad_LJPP;
+    }
+
+    public int getAniose_LJPP() {
+        return aniose_LJPP;
+    }
+
+    public void setAniose_LJPP(int aniose_LJPP) {
+        this.aniose_LJPP = aniose_LJPP;
+    }
     
 }
---
+++ src/main/java/com/mycompany/gestionmedica_ljpp/Paciente_LJPP.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gestionmedica_ljpp;
+
+/**
+ *
+ * @author perez
+ */
+public class Paciente_LJPP extends Persona_LJPP {
+    private int NhISTORIAL_LJPP;
+    private String enfermedad_LJPP;
+
+    public Paciente_LJPP(int NhISTORIAL_LJPP, String enfermedad_LJPP, String nombre_LJPP, int edad_LJPP, String direccion_LJPP) {
+        super(nombre_LJPP, edad_LJPP, direccion_LJPP);
+        this.NhISTORIAL_LJPP = NhISTORIAL_LJPP;
+        this.enfermedad_LJPP = enfermedad_LJPP;
+    }
+
+    public int getNhISTORIAL_LJPP() {
+        return NhISTORIAL_LJPP;
+    }
+
+    public void setNhISTORIAL_LJPP(int NhISTORIAL_LJPP) {
+        this.NhISTORIAL_LJPP = NhISTORIAL_LJPP;
+    }
+
+    public String getEnfermedad_LJPP() {
+        return enfermedad_LJPP;
+    }
+
+    public void setEnfermedad_LJPP(String enfermedad_LJPP) {
+        this.enfermedad_LJPP = enfermedad_LJPP;
+    }
+
+}
